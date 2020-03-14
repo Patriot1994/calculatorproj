77eb3d11566a1e1405ee7dce64f10c90a54b1bde
Patriot1994 <nowikpawel1994@gmail.com>
2020-03-14 19:04:51 +0100

    calculator_changeList

--- src/main/java/com/calculator/calculator/Controller/CalculatorController.java
+++ src/main/java/com/calculator/calculator/Controller/CalculatorController.java
@@ -8,7 +8,7 @@ import java.net.URLEncoder;
 
 @RestController
 @RequestMapping
-@CrossOrigin(origins = "https://calculatorpatriot1994.herokuapp.com/calculator") // http://localhost:4200
+@CrossOrigin(origins = "https://patriot1994.github.io/portfolio/calculator/") // http://localhost:4200
 public class CalculatorController {
     @RequestMapping(value="/calculator", method = RequestMethod.GET)
     public String calculate(@RequestParam(value = "equations",defaultValue = "0") String equations) throws UnsupportedEncodingException {
